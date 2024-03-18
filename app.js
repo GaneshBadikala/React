6cb6d856620714a2b8079f9b15bbb7c0f1d15743
Ganesh Badikala <ganesh.badikalai@vnd.freshworks.com>
2024-03-18 12:04:58 +0530

    Initail commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1",{id:"name"},"Ganesh Badikala")  
+console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById("main"))
+root.render(heading)
