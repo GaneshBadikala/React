8a359677676a1f31a6100abb704571cec2d5ca14
Ganesh Badikala <ganesh.badikalai@vnd.freshworks.com>
2024-11-04 09:33:43 +0530

    redo

--- app.js
+++
@@ -1,4 +0,0 @@
-const heading = React.createElement("h1",{id:"name"},"Ganesh Badikala")  
-console.log(heading)
-const root = ReactDOM.createRoot(document.getElementById("main"))
-root.render(heading)
