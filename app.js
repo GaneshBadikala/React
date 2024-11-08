14c10e4af28b7593f25af0b3654f2a4e1d87a996
Ganesh Badikala <ganesh.badikalai@vnd.freshworks.com>
2024-11-08 10:06:16 +0530

    installed parcel

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+
+
 const heading = React.createElement(
     "h1",
     {
@@ -6,20 +11,14 @@ const heading = React.createElement(
     "Heading 1"
   );
   
-  const heading2 = React.createElement(
-    "h2",
-    {
-      id: "title",
-    },
-    "Heading 2"
-  );
+
   
   const container = React.createElement(
     "div",
     {
       id: "container",
     },
-    [heading, heading2]
+    [heading]
   );
   
   console.log(heading);
@@ -30,4 +29,4 @@ const heading = React.createElement(
   
   //async defer
   root.render(container);
-  
+   
