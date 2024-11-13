3f8c42dfae554b2277e163427e95d3fe9ccae180
Ganesh Badikala <ganesh.badikalai@vnd.freshworks.com>
2024-11-13 21:54:25 +0530

    use of hooks

--- Food Delivery App/src/components/Body.js
+++ Food Delivery App/src/components/Body.js
@@ -1,26 +1,35 @@
 import RestCard from "./RestCard";
-import {restaurants} from "../utils/testData";
-import React, { useState } from 'react';
+import { restaurants } from "../utils/testData";
+import React, { useState, useEffect } from 'react';
 console.log(restaurants)
 
 const Body = () => {
-    const [restaurantsList,setrestaurantsList]= useState(restaurants)
+    const [restaurantsList, setrestaurantsList] = useState(restaurants)
+    const [searchValue, setsearchValue] = useState("")
+    useEffect(()=>{
+        let topRestaurents = restaurants.filter((res) =>res.info.name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+        setrestaurantsList(topRestaurents)
+    },[searchValue])
 
     return (
         <div className="mainBody">
             <div className="searchContainer">
                 <div className="searchBar">
-                    <input type="text" className="searchValue" placeholder="Search for restaurants and food"  ></input>
-                    <button id="searchButton"  onClick={()=>{
-                        let topRestaurents=restaurants.filter((res)=> res.info.avgRating > 4.5)
-                        setrestaurantsList(topRestaurents)
-                    }}>Search</button>
+                <input 
+                        type="text" 
+                        className="searchValue" 
+                        placeholder="Search for restaurants and food"
+
+                        onChange={(e) => setsearchValue(e.target.value)} // Update searchValue on change
+                    />
+                    <button id="searchButton" >Search</button>
                 </div>
             </div>
             <div className="res-container">
-                {restaurantsList.map((res) => 
-                        <RestCard key ={res.info.id} resData={res.info}/>
-                    )
+                {restaurantsList.map((res) =>
+                    <RestCard key={res.info.id} resData={res.info} />
+                )
                 }
 
             </div>
